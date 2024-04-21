8dfbac8ee50e4c08a5b9c25c8b68e65d92b9e048
smloring <156940158+smloring@users.noreply.github.com>
2024-04-21 19:03:19 +0000

    arrays submitted

--- MyClass.js
+++ MyClass.js
@@ -1,3 +1,43 @@
 /**
  * Add code for your class constructor in this file
  */
+// the Name of a constructor class should be capitalized
+function MyClass (tempX, tempY){ 
+    // this constructor expects two arguments
+    // all object properties and methods begin with "this."
+    this.x = tempX; // assign 1st argument to this.x 
+    this.y = tempY; // assign 2nd argument to this.y
+    this.d = 50; // initialize .d property to 50
+    this.speed = random(-1, 1); // initialize .speed to a random number between 1 and -1
+  
+    // methods are functions that are assigned to property names
+    this.move = function() {
+    // define how the object will change location on screen each frame
+      this.x += this.speed; // move by the value of .speed
+      if (this.x > width || this.x < 0){
+      // if the object reaches the right edge OR left edge
+        this.speed = this.speed * -1; // reverse polarity!
+      }
+    }
+    this.display = function(){ 
+      // define how the object will look
+      push();
+      translate(this.x, this.y) // center the drawing at the .x and .y properties of the object
+      fill(255, 100, 200); // red
+      ellipse(0, 0, this.d, this.d); // draw a circle at the center of the object image
+      pop();
+    
+
+      // Draw eyes
+  fill(0); // set fill color to black
+  ellipse(this.x - 10, this.y - 10, 10, 10); // left eye
+  ellipse(this.x + 10, this.y - 10, 10, 10); // right eye
+
+  // Draw mouth
+  fill(255, 192, 203); // set fill color to orange
+  triangle(this.x, this.y + 5, this.x - 5, this.y + 15, this.x + 5, this.y + 15); // nose
+
+
+};
+    }
+ // end of MyClass() constructor
--- script.js
+++ script.js
@@ -9,18 +9,34 @@
  * Make an array of objects of the same type. Start by creating an object constructor funciton. Test it with individual object instances. Then create an array and initialize it with objects created from your constructor.
  * Use the draw() function to display and move your objects independently on the canvas.
 ***/
+var objects = new Array(10); // the argument to Array() defines its size
 
 // Global Variables go here
+var obj1, obj2;
 
 function setup(){
   // this function will run once
   createCanvas(600, 400); // create a 600 x 400 pixel drawing canvas
+    obj1 = new MyClass(100, 100);
+    obj2 = new MyClass(200, 100);
+    for(var i = 0; i < objects.length; i++) {
+      objects[i] = new MyClass(random(width), random(height));
+      }
+    }
 
 
-}
+
 
 function draw(){
-  background(200); //light gray background
-  
+  background(115, 147, 179); //blue gray background
+  obj1.move(); // move object 1 (obj1)
+  obj1.display(); // display obj1 on screen
+  obj2.move();
+  obj2.display();
+  for(var i = 0; i < objects.length; i++){
+    objects[i].move(); // each time through the loop, move the next object in the array
+    objects[i].display(); // call the display method for each object (0 - 9)
+  }
 }
+
 
